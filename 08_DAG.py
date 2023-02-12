29b6c61671dbba34fee5f3a9cc980280a9a212f6
train centos7 <train@vbo.local>
2023-02-12 19:40:46 +0100

    First dag has created

--- 08_DAG.py
+++ 08_DAG.py
@@ -1,31 +1,27 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.operators.dummy import DummyOperator
+import Scraper
+import Spark as sp
 
+from airflow.operators.python import PythonOperator
+
+start_date = datetime(2022, 11, 5)
+current_ts = datetime.now().strftime('%Y%m%d_%H%M%S')
 default_args = {
-    'owner': 'karides',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 30),
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'owner': 'train',
+    'start_date': start_date,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(seconds=5)
 }
 
-def Departures_Istanbul():
-    # Insert your python code here
-    print("Departures of Istanbul are getting...")
+with DAG('gonnaflynow', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
 
-dag = DAG(
-    'Departures_Istanbul',
-    default_args=default_args,
-    schedule_interval='*/10 * * * *', # every 10 minutes
-    catchup=False,
-)
+    start = DummyOperator(task_id="start")
+    spark_building = PythonOperator(task_id='Spark_Building', python_callable=sp.Spark_builder())
+    call_scraper = PythonOperator(task_id='call_scraper', python_callable=sp.call_scraper(location='frankfurt'))
+    read_postgres = PythonOperator(task_id='read_postgres', python_callable=sp.read_postres())
+    write_postgres = PythonOperator(task_id='write_postgres', python_callable=sp.write_postgres())
+    stop = DummyOperator(task_id="stop")
 
-run_python_script_task = PythonOperator(
-    task_id='Departures_Istanbul',
-    python_callable=Departures_Istanbul,
-    dag=dag,
-)
-
+    start >> spark_building >> call_scraper >> read_postgres >> write_postgres >> stop
